bc3fdd037559fa9392a172e685692e03b87ee903
HarleyQueen <HarleyQueen@DESKTOP-TB45PB3>
2023-05-16 17:34:07 +0800

    ASA

---
+++ Form4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FamilyFeud
+{
+    public partial class Form4 : Form
+    {
+        public Form4(string top1, string top2, string top3, string top4, string top5, string top6, string top7, string top8, string top9, string top10)
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
